54f75ed3f8f824f0146005899b334c6260f5e06d
kgc00Zenbook <kirbygc00@gmail.com>
2019-05-21 23:00:03 -0700

    initial commit

---
+++ Assets/Scripts/Controller/BattleController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+public class BattleController : StateMachine {
+    public CameraRig cameraRig;
+    public Board board;
+    public LevelData levelData;
+    public Transform tileSelectionIndicator;
+    public Point pos;
+    public GameObject heroPrefab;
+    public Unit currentUnit;
+    public Tile currentTile { get { return board.GetTile (pos); } }
+    void Start () {
+        ChangeState<InitBattleState> ();
+    }
+}
